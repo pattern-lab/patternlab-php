395c0c3b8ca7081ee31e9b38a0fe85c0f3056057
Dave Olsen <dmolsen@gmail.com>
2013-06-21 20:38:00 -0400

    bad line break

--- builder/lib/builder.lib.php
+++ builder/lib/builder.lib.php
@@ -212,8 +212,7 @@ class Builder {
 	protected function gatherData() {
 		
 		// gather the data from the main source data.json
-		if (file_exists(__DIR__."/../../
-		source/data/data.json")) {
+		if (file_exists(__DIR__."/../../source/data/data.json")) {
 			$this->d = (object) array_merge(array(), (array) json_decode(file_get_contents(__DIR__."/../../source/data/data.json")));
 		}
 		
