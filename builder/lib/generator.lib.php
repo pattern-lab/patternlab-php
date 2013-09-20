e5bb74d0699d8080d9da8f01a541474c44fe0c5d
Dave Olsen <dmolsen@gmail.com>
2013-09-20 11:25:10 -0400

    updating the version number

--- builder/lib/generator.lib.php
+++ builder/lib/generator.lib.php
@@ -1,7 +1,7 @@
 <?php
 
 /*!
- * Pattern Lab Generator Class - v0.3.1
+ * Pattern Lab Generator Class - v0.3.2
  *
  * Copyright (c) 2013 Dave Olsen, http://dmolsen.com
  * Licensed under the MIT license
