5e9f74932ed74d2af484d91cd0795769619c5245
Dave Olsen <dmolsen@gmail.com>
2013-09-27 10:42:10 -0400

    updating the builder version

--- builder/lib/builder.lib.php
+++ builder/lib/builder.lib.php
@@ -1,7 +1,7 @@
 <?php
 
 /*!
- * Pattern Lab Builder Class - v0.3.2
+ * Pattern Lab Builder Class - v0.3.3
  *
  * Copyright (c) 2013 Dave Olsen, http://dmolsen.com
  * Licensed under the MIT license
