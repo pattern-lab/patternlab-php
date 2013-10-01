9603db37572a05950097a5adfe64a74afa846635
Dave Olsen <dmolsen@gmail.com>
2013-10-01 08:22:42 -0400

    removing requirement for jquery

--- public/styleguide/js/annotations-pattern.js
+++ public/styleguide/js/annotations-pattern.js
@@ -1,5 +1,5 @@
 /*!
- * Annotations Support for Patterns - v0.1
+ * Annotations Support for Patterns - v0.2
  *
  * Copyright (c) 2013 Dave Olsen, http://dmolsen.com
  * Licensed under the MIT license
@@ -10,43 +10,66 @@ var annotationsPattern = {
 	
 	commentsActive: false,
 	
+	/**
+	* add an onclick handler to each element in the pattern that has an annotation
+	*/
 	showComments: function() {
 		
-		$.each(comments.comments, function(i, item) {
-			$(item.el).bind('click',function(e) {
-			    if (annotationsPattern.commentsActive) {
-					e.preventDefault();
-					var obj = { "el": item.el, "title": item.title, "comment": item.comment };
-					parent.postMessage(obj,window.location.protocol+"//"+window.location.host);
-					return false;
+		for (comment in comments.comments) {
+			var item = comments.comments[comment];
+			var els  = document.querySelectorAll(item.el);
+			if (els.length > 0) {
+				for (el in els) {
+					els[el].onclick = (function(item) {
+						return function(e) {
+							if (annotationsPattern.commentsActive) {
+								e.preventDefault();
+								e.stopPropagation();
+								var obj = { "el": item.el, "title": item.title, "comment": item.comment };
+								var targetOrigin = (window.location.protocol == "file:") ? "*" : window.location.protocol+"//"+window.location.host;
+								parent.postMessage(obj,targetOrigin);
+							}
+					    }
+					  })(item);
 				}
-			});
-			
-		});
+			}
+		}
 		
 	},	
 	
-	
-	// watch the iframe source so that it can be sent back to everyone else.
-	// based on the great MDN docs at https://developer.mozilla.org/en-US/docs/Web/API/window.postMessage
+	/**
+	* toggle the has-comment class on/off based on user clicking in the viewer UI
+	* based on the great MDN docs at https://developer.mozilla.org/en-US/docs/Web/API/window.postMessage
+	* @param  {Object}      event info
+	*/
 	receiveIframeMessage: function(event) {
 		
-		if (event.data == "on") {
-			annotationsPattern.commentsActive = true;
-			$.each(comments.comments, function(i, item) {
-				$(item.el).addClass('has-comment');
-			});
-		} else if (event.data == "off") {
-			annotationsPattern.commentsActive = false;
-			$.each(comments.comments, function(i, item) {
-				$(item.el).removeClass('has-comment');
-			});
+		// does the origin sending the message match the current host? if not dev/null the request
+		if ((window.location.protocol != "file:") && (event.origin !== window.location.protocol+"//"+window.location.host)) {
+			return;
 		}
 		
+		if (event.data.commentToggle != undefined) {
+			annotationsPattern.commentsActive = (event.data.commentToggle == "on") ? true : false;
+			for (comment in comments.comments) {
+				var item = comments.comments[comment];
+				var els = document.querySelectorAll(item.el);
+				if (els.length > 0) {
+					for (el in els) {
+						if (els[el].classList.length > 0) {
+							if (event.data.commentToggle == "on") {
+								els[el].classList.add("has-comment");
+							} else {
+								els[el].classList.remove("has-comment");
+							}
+						}
+					}
+				}
+			}
+		}
 	}
-	
 	
 };
 
-$(document).ready(function() { annotationsPattern.showComments(); });
+annotationsPattern.showComments();
 window.addEventListener("message", annotationsPattern.receiveIframeMessage, false);
