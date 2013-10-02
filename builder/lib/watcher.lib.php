1bdbc1f726054c1b04eaa0b5b48dd5edc3b905f7
Dave Olsen <dmolsen@gmail.com>
2013-10-01 20:31:09 -0400

    updating the version number to v0.3.4

--- builder/lib/watcher.lib.php
+++ builder/lib/watcher.lib.php
@@ -1,7 +1,7 @@
 <?php
 
 /*!
- * Pattern Lab Watcher Class - v0.3.3
+ * Pattern Lab Watcher Class - v0.3.4
  *
  * Copyright (c) 2013 Dave Olsen, http://dmolsen.com
  * Licensed under the MIT license
