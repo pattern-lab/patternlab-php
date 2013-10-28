f71973c338c3cd7c1f1a6553ff6044f2b4a0d0ca
Dave Olsen <dmolsen@gmail.com>
2013-10-28 17:01:53 -0400

    making sure code view is turned off if annotations are turned on

--- public/styleguide/js/annotations-viewer.js
+++ public/styleguide/js/annotations-viewer.js
@@ -17,7 +17,15 @@ var annotationsViewer = {
 		$('body').addClass('comments-ready');
 		$('#sg-t-annotations').click(function(e) {
 			
-			e.preventDefault();		
+			e.preventDefault();
+			
+			// make sure the code view overlay is off
+			$('#sg-t-code').removeClass('active');
+			codeViewer.codeActive = false;
+			var targetOrigin = (window.location.protocol == "file:") ? "*" : window.location.protocol+"//"+window.location.host;
+			document.getElementById('sg-viewport').contentWindow.postMessage({ "codeToggle": "off" },targetOrigin);
+			codeViewer.slideCode(999);
+				
 			annotationsViewer.toggleComments();
 			annotationsViewer.commentContainerInit();
 			
@@ -100,7 +108,9 @@ var annotationsViewer = {
 		
 		if (event.data.commentOverlay != undefined) {
 			if (event.data.commentOverlay == "on") {
+				
 				annotationsViewer.updateComment(event.data.el,event.data.title,event.data.comment);
+				
 			} else {
 				annotationsViewer.slideComment($('#sg-annotation-container').outerHeight());
 			}
