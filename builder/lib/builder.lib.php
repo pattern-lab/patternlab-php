7af243c7be41eba55d7ff65d485d65d1fe942d88
Dave Olsen <dmolsen@gmail.com>
2013-10-28 22:51:19 -0400

    making the MQ regex more robust

--- builder/lib/builder.lib.php
+++ builder/lib/builder.lib.php
@@ -372,7 +372,7 @@ class Buildr {
 		
 		foreach(glob(__DIR__."/../../source/css/*.css") as $filename) {
 			$data    = file_get_contents($filename);
-			preg_match_all("/(min|max)-width:( |)(([0-9]{1,5})(\.[0-9]{1,20}|)(px|em))/",$data,$matches);
+			preg_match_all("/(min|max)-width:([ ]+)?(([0-9]{1,5})(\.[0-9]{1,20}|)(px|em))/",$data,$matches);
 			foreach ($matches[3] as $match) {
 				if (!in_array($match,$mqs)) {
 					$mqs[] = $match;
