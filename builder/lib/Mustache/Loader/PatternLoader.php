557c020a9da17a57f1953c9cdea34a99a2348f7e
Dave Olsen <dmolsen@gmail.com>
2013-11-29 13:27:07 -0500

    updating support for pattern parameters including support for a style modifier

--- builder/lib/Mustache/Loader/PatternLoader.php
+++ builder/lib/Mustache/Loader/PatternLoader.php
@@ -102,39 +102,46 @@ class Mustache_Loader_PatternLoader implements Mustache_Loader
      */
     protected function loadFile($name)
     {
-        //get pattern data 
-        $pattern_data = $this->getPatternIncludeData($name);
         
-        //get file path
-        $fileName     = $this->getFileName($pattern_data['file']);
-
-        //throw error if path is not found
+        // get pattern data
+        list($partialName,$styleModifier,$parameters) = $this->getPartialInfo($name);
+        
+        // get the real file path for the pattern
+        $fileName = $this->getFileName($partialName);
+        
+        // throw error if path is not found
         if (!file_exists($fileName)) {
             throw new Mustache_Exception_UnknownTemplateException($name);
         }
-
-        //get file as string
-        $file_str     = file_get_contents($fileName);
         
-        //if pattern include was called with param, render param
-        if (isset($pattern_data['param'])) {
-            $file_str = $this->renderPattern($file_str, $pattern_data['param']);
+        // get the file data
+        $fileData = file_get_contents($fileName);
+        
+        // if the pattern name had a style modifier find & replace it
+        if (count($styleModifier) > 0) {
+            $fileData = $this->findReplaceParameters($fileData, $styleModifier);
         }
         
-        return $file_str;
+        // if the pattern name had parameters find & replace them
+        if (count($parameters) > 0) {
+            $fileData = $this->findReplaceParameters($fileData, $parameters);
+        }
+        
+        return $fileData;
+        
     }
-
+    
     /**
      * Helper function for getting a Mustache template file name.
+     * @param  {String}       the pattern type for the pattern
+     * @param  {String}       the pattern sub-type
      *
-     * @param string $name
-     *
-     * @return string Template file name
+     * @return {Array}        an array of rendered partials that match the given path
      */
     protected function getFileName($name)
     {
         $fileName = "";
-
+        
         // test to see what kind of path was supplied
         $posDash  = strpos($name,"-");
         $posSlash = strpos($name,"/");
@@ -154,7 +161,7 @@ class Mustache_Loader_PatternLoader implements Mustache_Loader
                   }
               }
            }
-        
+           
         } else {
            $fileName = $this->baseDir."/".$name;
         }
@@ -164,136 +171,165 @@ class Mustache_Loader_PatternLoader implements Mustache_Loader
         }
         
         return $fileName;
+        
     }
     
     /**
-     * Inspects pattern include call for param
-     * ex:
-     * {{ > patternType-pattern(param1: value, param2 ...) }}
-     * 
-     * and returns pattern data array with the following elements
-     * 
-     * string $file - the file name
-     * 
-     * array|null $param - an associative array of param ex: param_name => $param_value... or null if none found
-     * 
-     * @param string $pattern_include_call_str 
-     * @return array returns pattern data array
-     * 
+     * Helper function to return the parts of a partial name
+     * @param  {String}       the name of the partial
+     *
+     * @return {Array}        the pattern type and the name of the pattern
      */
-    
-    protected function getPatternIncludeData($pattern_include_call_str)
+    private function getPatternInfo($name) 
     {
-        //create an array to store pattern include data
         
-        $pattern_data = array();
+        $patternBits = explode("-",$name);
         
-        // $pattern_include_call_str contains pattern name
-        // followed by param, example:
-        // patterntype-pattern (param1: value, param2: value2)
-        
-        $p_inc_str    = $pattern_include_call_str;
-        
-        //get param using regex
-        //first remove all line breaks from string 
-        
-        $p_inc_str    = preg_replace("/[\r\n]*/","",$p_inc_str);
-        
-        //now use regex to get everything between parentheses ( )
-        //note: this regex call ignores escaped parenteses \( and \)
-        
-        $matches      = preg_match('/\([^\(\\\\]*(?:\\\\.[^\)\\\\]*)*\)/s', $p_inc_str, $p_inc_param);
-        
-        //if no matches are found return file name only, and set
-        //param to null
-        
-        if(!$matches){
-            
-            $pattern_data['file']  = $p_inc_str;
-            $pattern_data['param'] = null;
-            return $pattern_data;
+        $i = 1;
+        $k = 2;
+        $c = count($patternBits);
+        $patternType = $patternBits[0];
+        while (!isset($this->patternPaths[$patternType]) && ($i < $c)) {
+            $patternType .= "-".$patternBits[$i];
+            $i++;
+            $k++;
         }
         
-        //otherwise strip param from string and set file
+        $patternBits = explode("-",$name,$k);
+        $pattern = $patternBits[count($patternBits)-1];
         
-        else {
-
-            //strip param and add [file] as the include file name 
-            $pattern_data['file']  = trim(preg_replace('/\([^\(\\\\]*(?:\\\\.[^\)\\\\]*)*\)/s', '', $p_inc_str));
-        }
+        return array($patternType, $pattern);
         
-        //remove spaces & parentheses from either end of param string
-        
-        $p_inc_param  = trim($p_inc_param[0], '()');
-        
-        //split param by commas , but ignore escaped commas
-        
-        $p_inc_param = preg_split('/(?<!\\\),/', $p_inc_param);
-        
-        //unescape param values, remove back slashes from parenthesis and commas 
-        
-        $p_inc_param = str_replace(array("\(", "\)", "\,"), array("(", ")", ","), $p_inc_param);
-        
-        //loop through param and set $pattern_data[param] with values
-        
-        foreach($p_inc_param as $arg)
-        {
-            //separate args by the first colon :
-            //ex param_name: param_value
-            
-            $r = explode(":", trim($arg), 2);
-            
-            //set pattern_data['param']['param_name'] with 
-            //the un escaped values (remove slashes from commas and )
-            $pattern_data['param'][$r[0]] = $r[1];
-        }
-        
-        return $pattern_data;
     }
     
     /**
-     * will replace each instance of {{ param_name }} with actual value 
-     * 
-     * @param string $pattern_string string to be parsed
-     * @param array $param an array of parameters
-     * @return string parsed $pattern_string 
+     * Helper function for finding if a partial name has style modifier or parameters
+     * @param  {String}       the pattern name
+     *
+     * @return {Array}        an array containing just the partial name, a style modifier, and any parameters
      */
-    
-    function renderPattern($pattern_string, $param)
+    protected function getPartialInfo($partial)
     {
-        //loop through given param
         
-        foreach($param as $k => $v) 
-        { 
-            //replace each instance of {{ param_name }} with actual value 
+        $styleModifier = array();
+        $parameters    = array();
+        
+        if (strpos($partial, "(") !== false) {
+            $partialBits      = explode("(",$partial,2);
+            $partial          = trim($partialBits[0]);
+            $cleanParameters   = explode(")",$partialBits[1]);
+            array_pop($cleanParameters);
+            $parametersString = implode($cleanParameters);
+            $parameters       = $this->parseParameters($parametersString);
+        }
+        
+        if (strpos($partial, ":") !== false) {
+            $partialBits      = explode(":",$partial,2);
+            $partial          = $partialBits[0];
+            $styleModifier    = array("styleModifier" => $partialBits[1]);
+        }
+        
+        return array($partial,$styleModifier,$parameters);
+        
+    }
+    
+    /**
+     * Helper function to find and replace the given parameters in a particular partial before handing it back to Mustache
+     * @param  {String}       the file contents
+     * @param  {Array}        an array of paramters to match
+     *
+     * @return {String}       the modified file contents
+     */
+    private function findReplaceParameters($fileData, $parameters)
+    {
+        foreach ($parameters as $k => $v) { 
+            $fileData = preg_replace('/{{([\s]*'.$k .'[\s]*)}}/', $v, $fileData);
+        }
+        return $fileData;
+    }
+    
+    /**
+     * Helper function to parse the parameters and return them as an array
+     * @param  {String}       tbe parameter string
+     *
+     * @return {Array}        the keys and values for the parameters
+     */
+    private function parseParameters($string) 
+    {
+        
+        $parameters     = array();
+        $betweenSQuotes = false;
+        $betweenDQuotes = false;
+        $inKey          = true;
+        $inValue        = false;
+        $char           = "";
+        $buffer         = "";
+        $keyBuffer      = "";
+        $strLength      = strlen($string);
+        
+        for ($i = 0; $i < $strLength; $i++) {
             
-            $pattern_string = preg_replace('/{{([\s]*'.$k .'[\s]*)}}/', $v, $pattern_string);
-        } 
+            $previousChar = $char;
+            $char = $string[$i];
+            
+            if ($inKey && !$betweenDQuotes && !$betweenSQuotes && (($char == "\"") || ($char == "'"))) {
+                // if inKey, a quote, and betweenQuotes is false ignore quote, set betweenQuotes to true and empty buffer to kill spaces
+                ($char == "\"") ? ($betweenDQuotes = true) : ($betweenSQuotes = true);
+            } else if ($inKey && (($betweenDQuotes && ($char == "\"")) || ($betweenSQuotes && ($char == "'"))) && ($previousChar == "\\")) {
+                // if inKey, a quote, betweenQuotes is true, and previous character is \ add to buffer
+                $buffer   .= $char;
+            } else if ($inKey && (($betweenDQuotes && ($char == "\"")) || ($betweenSQuotes && ($char == "'")))) {
+                // if inKey, a quote, betweenQuotes is true set betweenQuotes to false, save as key buffer, empty buffer set inKey false
+                $keyBuffer = $buffer;
+                $buffer    = "";
+                $inKey     = false;
+                $betweenSQuotes = false;
+                $betweenDQuotes = false;
+            } else if ($inKey && !$betweenDQuotes && !$betweenSQuotes && ($char == ":")) {
+                // if inKey, a colon, betweenQuotes is false, save as key buffer, empty buffer, set inKey false set inValue true
+                $keyBuffer = $buffer;
+                $buffer    = "";
+                $inKey     = false;
+                $inValue   = true;
+            } else if ($inKey) {
+                // if inKey add to buffer
+                $buffer   .= $char;
+            } else if (!$inKey && !$inValue && ($char == ":")) {
+                // if inKey is false, inValue false, and a colon set inValue true
+                $inValue = true;
+            } else if ($inValue && !$betweenDQuotes && !$betweenSQuotes && (($char == "\"") || ($char == "'"))) {
+                // if inValue, a quote, and betweenQuote is false set betweenQuotes to true and empty buffer to kill spaces
+                ($char == "\"") ? ($betweenDQuotes = true) : ($betweenSQuotes = true);
+            } else if ($inValue && (($betweenDQuotes && ($char == "\"")) || ($betweenSQuotes && ($char == "'"))) && ($previousChar == "\\")) {
+                // if inValue, a quote, betweenQuotes is true, and previous character is \ add to buffer
+                $buffer   .= $char;
+            } else if ($inValue && (($betweenDQuotes && ($char == "\"")) || ($betweenSQuotes && ($char == "'")))) {
+                // if inValue, a quote, betweenQuotes is true set betweenQuotes to false, save to parameters array, empty buffer, set inValue false
+                $buffer    = str_replace("\\\"","\"",$buffer);
+                $buffer    = str_replace('\\\'','\'',$buffer);
+                $parameters[trim($keyBuffer)] = trim($buffer);
+                $buffer    = "";
+                $inValue   = false;
+                $betweenSQuotes = false;
+                $betweenDQuotes = false;
+            } else if ($inValue && !$betweenDQuotes && !$betweenSQuotes && ($char == ",")) {
+                // if inValue, a comman, betweenQuotes is false, save to parameters array, empty buffer, set inValue false, set inKey true
+                $parameters[trim($keyBuffer)] = trim($buffer);
+                $buffer    = "";
+                $inValue   = false;
+                $inKey     = true;
+            } else if ($inValue) {
+                // if inValue add to buffer
+                $buffer   .= $char;
+            } else if (!$inValue && !$inKey && ($char == ",")) {
+                // if inValue is false, inKey false, and a comma set inKey true
+                $inKey = true;
+            }
+            
+        }
         
-        //return new pattern string
+        return $parameters;
         
-        return $pattern_string;
-    }    
-
-    private function getPatternInfo($name) {
-	
-		$patternBits = explode("-",$name);
-		
-		$i = 1;
-		$k = 2;
-		$c = count($patternBits);
-		$patternType = $patternBits[0];
-		while (!isset($this->patternPaths[$patternType]) && ($i < $c)) {
-			$patternType .= "-".$patternBits[$i];
-			$i++;
-			$k++;
-		}
-		
-		$patternBits = explode("-",$name,$k);
-		$pattern = $patternBits[count($patternBits)-1];
-		
-		return array($patternType, $pattern);
-		
-	}
-	
+    }
+    
 }
