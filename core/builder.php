2b5dfd33a45ab8140f517da8ef0787890313637c
Dave Olsen <dmolsen@gmail.com>
2014-02-08 19:39:54 -0500

    add a check for json_decode support

--- core/builder.php
+++ core/builder.php
@@ -27,6 +27,12 @@
  *
  */
 
+// check to see if json_decode exists. might be disabled in installs of PHP 5.5
+if (!function_exists("json_decode")) {
+	print "Please check that your version of PHP includes the JSON extension. It's required for Pattern Lab to run. Aborting.\n";
+	exit;
+}
+
 // auto-load classes
 require(__DIR__."/lib/SplClassLoader.php");
 
