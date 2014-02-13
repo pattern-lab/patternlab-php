ec0f9a07b82e23103d9f312b5b8e656dc4e8ad5e
Dave Olsen <dmolsen@gmail.com>
2014-02-12 22:39:52 -0500

    watcher fix for windows

--- core/lib/PatternLab/Watcher.php
+++ core/lib/PatternLab/Watcher.php
@@ -35,7 +35,7 @@ class Watcher extends Builder {
 		
 		// automatically start the auto-refresh tool
 		if ($reload) {
-			$path = str_replace("lib/PatternLab","autoReloadServer.php",__DIR__);
+			$path = str_replace("lib".DIRECTORY_SEPARATOR."PatternLab","autoReloadServer.php",__DIR__);
 			$fp = popen("php ".$path." -s", "r"); 
 			print "starting page auto-reload...\n";
 		}
