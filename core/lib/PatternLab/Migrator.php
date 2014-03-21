fd9ed088baa0f0006ed426c6960826babb0a0c2c
Dave Olsen <dmolsen@gmail.com>
2014-03-21 08:16:45 -0400

    updating the version number

--- core/lib/PatternLab/Migrator.php
+++ core/lib/PatternLab/Migrator.php
@@ -1,7 +1,7 @@
 <?php
 
 /*!
- * Pattern Lab Migrator Class - v0.7.10
+ * Pattern Lab Migrator Class - v0.7.11
  *
  * Copyright (c) 2014 Dave Olsen http://dmolsen.com
  * Licensed under the MIT license
