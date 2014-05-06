d4b95e7ae62563cc6c4123793540f4d44f2d2085
Dave Olsen <dmolsen@gmail.com>
2014-05-06 15:52:59 -0400

    smarter handling of _blank & _parent

--- core/styleguide/js/postmessage.js
+++ core/styleguide/js/postmessage.js
@@ -30,10 +30,16 @@ if (self != top) {
 	var aTags = document.getElementsByTagName('a');
 	for (var i = 0; i < aTags.length; i++) {
 		aTags[i].onclick = function(e) {
-			e.preventDefault();
-			var href = this.getAttribute("href");
-			if (href != "#") {
+			var href   = this.getAttribute("href");
+			var target = this.getAttribute("target");
+			if ((target != undefined) && ((target == "_parent") || (target == "_blank"))) {
+				// just do normal stuff
+			} else if (href != "#") {
+				e.preventDefault();
 				window.location.replace(href);
+			} else {
+				e.preventDefault();
+				return false;
 			}
 		};
 	}
