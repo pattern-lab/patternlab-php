f4ac46312c2e262c43119ed2e1ff51fe2afb37dd
Dave Olsen <dmolsen@gmail.com>
2014-05-09 16:20:23 -0400

    adding starter kit support

--- core/lib/PatternLab/StarterKit.php
+++ core/lib/PatternLab/StarterKit.php
@@ -18,7 +18,11 @@ class StarterKit {
 	* Set-up a default var
 	*/
 	public function __construct($config) {
-		$this->sourceDir     = __DIR__."/../../../".$config["publicDir"];
+		$this->sourceDir     = __DIR__."/../../../".$config["sourceDir"];
+		if (!is_dir($this->sourceDir)) {
+			print "Check to make sure your source directory is configured properly...\n";
+			exit;
+		}
 	}
 	
 	/**
@@ -27,41 +31,114 @@ class StarterKit {
 	 *
 	 * @return {String}    the modified file contents
 	 */
-	public function fetch($org,$repo,$tag) {
+	public function fetch($starterKit) {
 		
-		//master
-		//tag
+		// see if the user passed anythign useful
+		if (empty($starterKit)) {
+			print "please provide a path for the starter kit before trying to fetch it...\n";
+			exit;
+		}
+		
+		// figure out the options for the GH path
+		list($org,$repo,$tag) = $this->getStarterKitInfo($starterKit);
 		
 		//get the path to the GH repo and validate it
-		$tarballUrl = "https://github.com/".$org."/".$repo."/archive/".$tag.".tar.gzz";
+		$tarballUrl = "https://github.com/".$org."/".$repo."/archive/".$tag.".tar.gz";
+		
+		print "downloading the starter kit...\n";
 		
 		// try to download the given starter kit
-		try {
-			$starterKit = file_get_contents($tarballUrl);
-		} catch (\Exception $e) {
-			throw new \Exception('Something really gone wrong'.$e->getMessage(), 0, $e);
+		if (!$starterKit = @file_get_contents($tarballUrl)) {
+			$error = error_get_last();
+			print "starter kit wasn't downloaded because:\n  ".$error["message"]."\n";
+			exit;
 		}
 		
 		// write the starter kit to the temp directory
 		$tempFile = tempnam(sys_get_temp_dir(), "pl-sk-archive.tar.gz");
 		file_put_contents($tempFile, $starterKit);
 		
-		$zip = new ZipArchive;
-		if ($zip->open('test.zip') === TRUE) {
-		    $zip->extractTo('/my/destination/dir/');
-		    $zip->close();
-		    echo 'ok';
-		} else {
-		    echo 'failed';
+		// see if the source directory is empty
+		$emptyDir = true;
+		$objects  = new \DirectoryIterator($this->sourceDir);
+		foreach ($objects as $object) {
+			if (!$object->isDot() && ($object->getFilename() != "README") && ($object->getFilename() != ".DS_Store")) {
+				$emptyDir = false;
+			}
 		}
 		
+		print "installing the starter kit...\n";
+		
+		// if source directory isn't empty ask if it's ok to nuke what's there
+		if (!$emptyDir) {
+			$stdin = fopen("php://stdin", "r");
+			print("delete everything in source/ before installing the starter kit? Y/n\n");
+			$answer = strtolower(trim(fgets($stdin)));
+			fclose($stdin);
+			if ($answer == "y") {
+				
+				print "nuking everything in source/...\n";
+				
+				$objects = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->sourceDir), \RecursiveIteratorIterator::CHILD_FIRST);
+				
+				// make sure dots are skipped
+				$objects->setFlags(\FilesystemIterator::SKIP_DOTS);
+				
+				foreach($objects as $name => $object) {
+					
+					if ($object->isDir()) {
+						rmdir($name);
+					} else if ($object->isFile()) {
+						unlink($name);
+					}
+					
+				}
+				
+			} else {
+				print "aborting install of the starter kit...\n";
+				unlink($tempFile);
+				exit;
+			}
+			
+		}
+		
+		// extract
+		$zippy      = \Alchemy\Zippy\Zippy::load();
+		$zipAdapter = $zippy->getAdapterFor('tar.gz');
+		$archiveZip = $zipAdapter->open($tempFile);
+		$archiveZip = $archiveZip->extract($this->sourceDir);
+		
+		// remove the temp file
 		unlink($tempFile);
 		
-		//see if source is empty, if not prompt if they want to delete stuff
-		//delete everything
-		//download
-		//unzip
-		//unpack into source
+		print "starter kit installation complete...\n";
+		
+	}
+	
+	/**
+	 * Break up the starterKit path
+	 * @param  {String}    path of the GitHub repo
+	 *
+	 * @return {Array}     the parts of the starter kit path
+	 */
+	protected function getStarterKitInfo($starterKit) {
+		
+		$org  = "";
+		$repo = "";
+		$tag  = "master";
+		
+		if (strpos($starterKit, "#") !== false) {
+			list($starterKit,$tag) = explode("#",$starterKit);
+		}
+		
+		if (strpos($starterKit, "/") !== false) {
+			list($org,$repo) = explode("/",$starterKit);
+		} else {
+			print "please provide a real path to a starter kit...\n";
+			exit;
+		}
+		
+		return array($org,$repo,$tag);
 		
 	}
 	
