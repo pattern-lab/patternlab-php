3ccba5bfcc74aa6bc05192e01e9aab6e926acc87
Dave Olsen <dmolsen@gmail.com>
2014-05-20 14:15:55 -0400

    copy edit

--- core/lib/PatternLab/TemplateHelper.php
+++ core/lib/PatternLab/TemplateHelper.php
@@ -25,13 +25,13 @@ class TemplateHelper {
 	public function __construct($sp) {
 		
 		// load pattern-lab's resources
-		$htmlHead  = file_get_contents(__DIR__."/../../templates/pattern-header-footer/header.html");
-		$htmlFoot  = file_get_contents(__DIR__."/../../templates/pattern-header-footer/footer.html");
-		$extraFoot = file_get_contents(__DIR__."/../../templates/pattern-header-footer/footer-pattern.html");
+		$htmlHead           = file_get_contents(__DIR__."/../../templates/pattern-header-footer/header.html");
+		$htmlFoot           = file_get_contents(__DIR__."/../../templates/pattern-header-footer/footer.html");
+		$extraFoot          = file_get_contents(__DIR__."/../../templates/pattern-header-footer/footer-pattern.html");
 		
 		// gather the user-defined header and footer information
-		$patternHeadPath = __DIR__.$sp."00-atoms/00-meta/_00-head.mustache";
-		$patternFootPath = __DIR__.$sp."00-atoms/00-meta/_01-foot.mustache";
+		$patternHeadPath    = __DIR__.$sp."00-atoms/00-meta/_00-head.mustache";
+		$patternFootPath    = __DIR__.$sp."00-atoms/00-meta/_01-foot.mustache";
 		$patternHead        = (file_exists($patternHeadPath)) ? file_get_contents($patternHeadPath) : "";
 		$patternFoot        = (file_exists($patternFootPath)) ? file_get_contents($patternFootPath) : "";
 		
