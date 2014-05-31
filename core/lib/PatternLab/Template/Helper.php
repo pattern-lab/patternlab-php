0a64d451a61737d14636ac7af8d0bda69c8de69d
Dave Olsen <dmolsen@gmail.com>
2014-05-30 22:36:39 -0400

    moving the template helper and making it static

---
+++ core/lib/PatternLab/Template/Helper.php
@@ -0,0 +1,56 @@
+<?php
+
+/*!
+ * Template Helper Class
+ *
+ * Copyright (c) 2014 Dave Olsen, http://dmolsen.com
+ * Licensed under the MIT license
+ *
+ * Set-ups the vars for the template loader
+ *
+ */
+
+namespace PatternLab\Template;
+
+use \PatternLab\Config;
+use \PatternLab\Template\Loader;
+
+class Helper {
+	
+	public static $patternHead;
+	public static $patternFoot;
+	public static $mainPageHead;
+	public static $mainPageFoot;
+	public static $filesystemLoader;
+	public static $htmlLoader;
+	
+	/**
+	* Set-up default vars
+	*/
+	public static function setup() {
+		
+		// load pattern-lab's resources
+		$htmlHead               = file_get_contents(__DIR__."/../../../templates/pattern-header-footer/header.html");
+		$htmlFoot               = file_get_contents(__DIR__."/../../../templates/pattern-header-footer/footer.html");
+		$extraFoot              = file_get_contents(__DIR__."/../../../templates/pattern-header-footer/footer-pattern.html");
+		
+		// gather the user-defined header and footer information
+		$patternHeadPath        = __DIR__."/..".Config::$options["patternSourceDir"]."00-atoms/00-meta/_00-head.mustache";
+		$patternFootPath        = __DIR__."/..".Config::$options["patternSourceDir"]."00-atoms/00-meta/_01-foot.mustache";
+		$patternHead            = (file_exists($patternHeadPath)) ? file_get_contents($patternHeadPath) : "";
+		$patternFoot            = (file_exists($patternFootPath)) ? file_get_contents($patternFootPath) : "";
+		
+		// add pattern lab's resource to the user-defined files
+		self::$patternHead      = str_replace("{% pattern-lab-head %}",$htmlHead,$patternHead);
+		self::$patternFoot      = str_replace("{% pattern-lab-foot %}",$extraFoot.$htmlFoot,$patternFoot);
+		self::$mainPageHead     = self::$patternHead;
+		self::$mainPageFoot     = str_replace("{% pattern-lab-foot %}",$htmlFoot,$patternFoot);
+		
+		// add the generic loaders
+		$templateLoader         = new Loader();
+		self::$filesystemLoader = $templateLoader->fileSystem();
+		self::$htmlLoader       = $templateLoader->vanilla();
+		
+	}
+	
+}
