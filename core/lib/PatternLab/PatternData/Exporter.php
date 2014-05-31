16211d907341e3cdd713c0b1ed141e117a4272f7
Dave Olsen <dmolsen@gmail.com>
2014-05-30 22:45:38 -0400

    generic exporter class

---
+++ core/lib/PatternLab/PatternData/Exporter.php
@@ -0,0 +1,21 @@
+<?php
+
+/*!
+ * Pattern Data Exporter Class
+ *
+ * Copyright (c) 2014 Dave Olsen, http://dmolsen.com
+ * Licensed under the MIT license
+ *
+ */
+
+namespace PatternLab\PatternData;
+
+class Exporter {
+	
+	public function __construct($options) {
+		
+		// nothing here yet
+		
+	}
+	
+}
