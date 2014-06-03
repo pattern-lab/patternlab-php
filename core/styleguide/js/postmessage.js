a1c0057f9a6a1dd7d19502388af5c2a77dfd2a3b
Dave Olsen <dmolsen@gmail.com>
2014-06-03 16:08:29 -0400

    another fix from @fstop

--- core/styleguide/js/postmessage.js
+++ core/styleguide/js/postmessage.js
@@ -34,7 +34,7 @@ if (self != top) {
 			var target = this.getAttribute("target");
 			if ((target != undefined) && ((target == "_parent") || (target == "_blank"))) {
 				// just do normal stuff
-			} else if (href.length && href !== "#") {
+			} else if (href && href !== "#") {
 				e.preventDefault();
 				window.location.replace(href);
 			} else {
