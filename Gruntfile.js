36118f5531e38986dcea69fca95bad2d98f123e9
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2015-04-28 11:35:39 +0200

    refactor Sass architecture

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,43 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    // Build Pattern Lab
+
+
+
+
+
+    clean: {
+      css: ['source/css/'],
+    },
+
+
+
+
+
+    sass: {
+      style: {
+        options: {
+          //style: 'compressed'
+          style: 'nested'
+        },
+        files: {
+          'source/css/style.css': 'source/_sass/style.scss'
+        }
+      },
+      'style-min': {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'source/css/style.min.css': 'source/_sass/style.scss'
+        }
+      },
+    },
+
+
+
+
+
     shell: {
       'patternlab': {
         command: "php core/builder.php -g"
@@ -14,6 +50,11 @@ module.exports = function(grunt) {
         command: "cp -rf core/styleguide public/styleguide"
       }
     },
+
+
+
+
+
     'gh-pages': {
       options: {
         base: 'public'
@@ -24,13 +65,25 @@ module.exports = function(grunt) {
 
   // Load the plugins
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-gh-pages');
-
-  // Default task(s).
-  grunt.registerTask('default', 'build Pattern Lab', ['shell:patternlab']);
 
   // Init Pattern Lab
   grunt.registerTask('init', 'Init Pattern lab',['shell:patternlab-public', 'shell:patternlab-styleguide'])
+
+  // Build stylesheets
+  grunt.registerTask('css', 'Build stylesheets', function() {
+    grunt.task.run('clean:css');
+    grunt.task.run('sass');
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', 'build Pattern Lab', function() {
+    grunt.log.subhead("Build everything !".magenta);
+    grunt.task.run('css');
+    grunt.task.run('shell_patternlab');
+  });
 
   // Init Pattern Lab
   grunt.registerTask('deploy', 'Deploy Pattern Lab on gh-pages',['gh-pages'])
